582ce62e086b43b983d54ac381afb4ef77bcf0b0
wwwkimminja <82824976+wwwkimminja@users.noreply.github.com>
2022-07-05 12:28:39 +0900

    first commit

---
+++ main.js
@@ -0,0 +1,4 @@
+const start = document.querySelector('.start');
+const time = document.querySelector('.time');
+const count = document.querySelector('.count');
+const canvas = document.querySelector('.canvas');
