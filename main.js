22b2f065ea106a54e5a2ea10119c4f970d1ab49f
wwwkimminja <82824976+wwwkimminja@users.noreply.github.com>
2022-07-05 14:03:25 +0900

    startGame()

--- main.js
+++ main.js
@@ -1,15 +1,51 @@
 // スクリプト全体の厳格モード構文
 'use strict';
 const CARROT_SIZE = 80;
+const CARROT_COUNT = 5;
+const BUG_COUNT = 5;
 const field = document.querySelector('.game__field');
 const fieldRect = field.getBoundingClientRect();
-const startGameBtn = document.querySelector('.game__button');
+const gameBtn = document.querySelector('.game__button');
+const gameTimer = document.querySelector('.game__timer');
+const gameScore = document.querySelector('.game__score');
 
+let started = false;
+let score = 0;
+let timer = undefined;
+
+gameBtn.addEventListener('click', () => {
+    if (started) {
+        stopGame();
+    } else {
+        startGame();
+    }
+    started = !started;
+});
+
+function startGame() {
+    initGame();
+    showStopButton();
+    showTimerAndScore();
+    startGameTimer();
+}
+function stopGame() {
+
+}
 function initGame() {
-    //게임 필드에 당근과 벌레 생성한뒤 필드에 추가해준다
-    console.log(fieldRect);
-    addItem('carrot', 5, 'img/carrot.png');
-    addItem('bug', 5, 'img/bug.png');
+    field.innerHTML = '';
+    gameScore.innerText = CARROT_COUNT;
+    addItem('carrot', CARROT_COUNT, 'img/carrot.png');
+    addItem('bug', BUG_COUNT, 'img/bug.png');
+}
+function showTimerAndScore() {
+    gameTimer.style.visibility = 'visible';
+    gameScore.style.visibility = 'visible';
+}
+function showStopButton() {
+    const icon = gameBtn.querySelector('.fa-play');
+    icon.classList.add('fa-stop');
+    icon.classList.remove('fa-play');
+
 }
 function addItem(className, count, imgPath) {
     const x1 = 0;
@@ -33,4 +69,3 @@ function randomNum(min, max) {
     return Math.random() * (max - min) + min;
 }
 
-initGame();
