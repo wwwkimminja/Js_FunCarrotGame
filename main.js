33e734eb84460fcec6e5e4177a84290159a718e8
wwwkimminja <82824976+wwwkimminja@users.noreply.github.com>
2022-07-05 13:18:46 +0900

    create carrots and bus

--- main.js
+++ main.js
@@ -1,4 +1,36 @@
-const start = document.querySelector('.start');
-const time = document.querySelector('.time');
-const count = document.querySelector('.count');
-const canvas = document.querySelector('.canvas');
+// スクリプト全体の厳格モード構文
+'use strict';
+const CARROT_SIZE = 80;
+const field = document.querySelector('.game__field');
+const fieldRect = field.getBoundingClientRect();
+const startGameBtn = document.querySelector('.game__button');
+
+function initGame() {
+    //게임 필드에 당근과 벌레 생성한뒤 필드에 추가해준다
+    console.log(fieldRect);
+    addItem('carrot', 5, 'img/carrot.png');
+    addItem('bug', 5, 'img/bug.png');
+}
+function addItem(className, count, imgPath) {
+    const x1 = 0;
+    const y1 = 0;
+    const x2 = fieldRect.width - CARROT_SIZE;
+    const y2 = fieldRect.height - CARROT_SIZE;
+    for (let i = 0; i < count; i++) {
+        const item = document.createElement('img');
+        item.setAttribute('class', className);
+        item.setAttribute('src', imgPath);
+        item.style.position = 'absolute';
+        const x = randomNum(x1, x2);
+        const y = randomNum(y1, y2);
+        item.style.left = `${x}px`;
+        item.style.top = `${y}px`;
+        field.appendChild(item);
+    }
+}
+
+function randomNum(min, max) {
+    return Math.random() * (max - min) + min;
+}
+
+initGame();
